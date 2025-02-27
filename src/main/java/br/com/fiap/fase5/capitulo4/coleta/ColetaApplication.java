c5405b29f9d38399d5998f3c98e220cf517d150a
Bruno Fernando <brunofernandodco@gmail.com>
2025-02-26 23:16:10 -0300

    scanBasePackages removido da anotação @SpringBootApplication, na classe main.

--- src/main/java/br/com/fiap/fase5/capitulo4/coleta/ColetaApplication.java
+++ src/main/java/br/com/fiap/fase5/capitulo4/coleta/ColetaApplication.java
@@ -3,7 +3,7 @@ package br.com.fiap.fase5.capitulo4.coleta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "br.com.fiap.fase5.capitulo4.coleta")
+@SpringBootApplication
 public class ColetaApplication {
 
 	public static void main(String[] args) {
